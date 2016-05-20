cbfae610527b9857f381cdce989f61b9bf113697
Cefyr <cefyr.mail@gmail.com>
2016-05-21 00:30:22 +0200

    untested config, class constructor doesn't like args

--- svipdagr.py
+++ svipdagr.py
@@ -4,18 +4,20 @@
 svipdagr is a systray-based program for knowing how late it is
 Original idea of code copypasted from http://rowinggolfer.blogspot.se/2011/06/pyqt-qsystrayicon-example.html
 """
-import sys
+import sys, json
 from PyQt4 import QtGui, QtCore
 
 
 class SysTrayIcon(QtGui.QSystemTrayIcon):
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, config_data):
         super().__init__()
+        self.config_data = config_data
         self.icon = self.init_icon()
-        self.gui = GUI()
+        self.gui = GUI(self.config_data)
 
         #self.setIcon(QtGui.QIcon.fromTheme("exit24.png"))
         self.setIcon(self.icon)
+        self.update_icon()
         self.setToolTip('This is a tooltip for the icon')
         self.activated.connect(self.click_trap)
 
@@ -43,9 +45,11 @@ class SysTrayIcon(QtGui.QSystemTrayIcon):
             else:
                 self.gui.show()
 
-    def welcome(self):
-        self.showMessage("Hello", "I should be aware of both buttons")
-        
+    def update_icon(self):
+        #TODO figure out icon color
+        #TODO repaint pixmap?
+        pass
+
     def show(self):
         QtGui.QSystemTrayIcon.show(self)
         #QtCore.QTimer.singleShot(100, self.welcome)
@@ -53,7 +57,7 @@ class SysTrayIcon(QtGui.QSystemTrayIcon):
 
 class GUI(QtGui.QWidget):
     #TODO Read everything from some decent config in Dropbox for good appiness
-    def __init__(self):
+    def __init__(self, config_data):
         super().__init__()
 
         # Night config widgets
@@ -72,7 +76,10 @@ class GUI(QtGui.QWidget):
         #tformat_vbox.addWidget(minute_rbtn)
         #tformat_vbox.addWidget(fraction_rbtn)
         #tformat_group.setLayout(tformat_vbox)
-        tformat_lbl = QtGui.QLabel('Tidsformat:')
+        tformat_lbl = QtGui.QLabel('Tidsformat, visning:')
+
+        apply_btn = QtGui.QButton('Verkställ')
+        apply_btn.clicked.connect(self.update)
 
         # Grid layout for everything
         grid = QtGui.QGridLayout()
@@ -80,7 +87,6 @@ class GUI(QtGui.QWidget):
 
         grid.addWidget(reveille_lbl, 1, 0)
         grid.addWidget(reveille_edit, 1, 1)
-
         grid.addWidget(optimum_lbl, 2, 0)
         grid.addWidget(optimum_edit, 2, 1)
 
@@ -88,11 +94,32 @@ class GUI(QtGui.QWidget):
         grid.addWidget(tformat_lbl, 3, 0)
         grid.addWidget(minute_rbtn, 3, 1)
         grid.addWidget(fraction_rbtn, 4, 1)
+        grid.addWidget(apply_btn, 5, 1)
 
         self.setLayout(grid)
 
         self.setWindowTitle('Svipdagr')
         #self.show()
+
+    def update(self):
+        # see the textEdited() signal for automating this
+        reveille_str = reveille_edit.text()
+        optimum_str = optimum_edit.text()
+        minute_format = minute_rbtn.isChecked()
+        # TODO clean data
+        reveille = reveille_str.strip()
+        optimum_sleep = optimum_str.strip()
+        color_early = [100, 100, 100]
+        color_late = [255, 255, 255]
+        config_data = {"reveille": reveille, 
+                       "optimum_sleep": optimum_sleep,
+                       "minute_format": minute_format,
+                       "color_early": color_early,
+                       "color_late": color_late}
+        config_file = os.path.join('~/.config/svipdagr', 'config.json')
+        write_config(config_file, config_data)
+
+        self.hide()
 
     def closeEvent(self, event):
         # 1st string is titlebar, 2nd is message, last param is default btn
@@ -127,9 +154,23 @@ class RightClickMenu(QtGui.QMenu): #NOTE Inactive right now
             event.ignore()
 
 
+def read_config():
+    config_file = '~/.config/svipdagr/config'
+    if not os.path.exists(config_file):
+        config_file = 'default_config.json'
+    with open(config_file) as json_file:
+        config_data = json.load(json_file)
+        return config_data
+
+def write_config(config_file, config_data):
+    with open(config_file, 'w') as outfile:
+        json.dump(config_data, outfile)
+
+
 def main():
     app = QtGui.QApplication(sys.argv)
-    tray = SysTrayIcon()
+    config_data = read_config()
+    tray = SysTrayIcon(config_data)
     tray.show()
     sys.exit(app.exec_())
 
